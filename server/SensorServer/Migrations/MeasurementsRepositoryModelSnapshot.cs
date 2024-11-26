b620f24b6a29335daf6899cc510f0c8df0b4f9f0
Florian Friedrich <ffried@me.com>
2024-11-26 09:08:08 +0100

    Update db context snapshot

--- server/SensorServer/Migrations/MeasurementsRepositoryModelSnapshot.cs
+++ server/SensorServer/Migrations/MeasurementsRepositoryModelSnapshot.cs
@@ -15,11 +15,11 @@ namespace SensorServer.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
+            modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
 
             modelBuilder.Entity("SensorServer.Models.Measurement", b =>
                 {
-                    b.Property<long?>("Id")
+                    b.Property<long>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
